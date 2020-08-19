4ae1f76a6d897094e50c38cfda7d20c673151f92
H <69875026+hackingconh@users.noreply.github.com>
2020-08-19 02:56:13 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 #Author: github.com/https://github.com/hackingconh
 #Instagram: @hacking.con.h
-
+white="\033[1;37m"
+grey="\033[0;37m"
+purple="\033[0;35m"
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+Purple="\033[0;35m"
+Cyan="\033[0;36m"
+Cafe="\033[0;33m"
+Fiuscha="\033[0;35m"
+blue="\033[1;34m"
+nc="\e[0m"
+mon="mon"
 
 #root privilegies
 if [[ $EUID -ne 0 ]]; then														
@@ -28,9 +40,9 @@ make
 make install
 #macchanger
 apt-get install macchanger
-
+echo -e "$green$
 echo " 
 
 
-TODO INSTALADO [$green✓$nc] "
+TODO INSTALADO [$blue✓$nc] "
 
